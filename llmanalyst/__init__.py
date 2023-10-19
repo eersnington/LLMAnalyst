a4e4b6387166352e82a897300b82972d202a7b6b
Eers <sreeaadhi07@gmail.com>
2023-10-19 10:51:20 +0530

    changes

--- llmanalyst/__init__.py
+++ llmanalyst/__init__.py
@@ -1,3 +1,3 @@
-from .llm_analyst import HuggingfaceAnalyst, OpenAIGPTAnalyst
+from llmanalyst.llm_analyst import HuggingfaceAnalyst, OpenAIGPTAnalyst
 
 __all__ = ['HuggingfaceAnalyst', 'OpenAIGPTAnalyst']
