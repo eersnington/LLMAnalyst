7a17c3ce9c517d9bb46f520fc012baded38fed02
Eers <sreeaadhi07@gmail.com>
2023-10-19 10:06:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=["transformers>=4.32.0","optimum>=1.12.0", "accelerate", "langchain",  "streamlit", "streamlit-chat", "pandas", "matplotlib"],
+    install_requires=["transformers>=4.32.0","optimum>=1.12.0", "langchain",  "streamlit", "streamlit-chat", "pandas", "matplotlib"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
